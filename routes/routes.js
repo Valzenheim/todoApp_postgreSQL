60d1015dd9236be6a56ccf77c441448b56d10765
Mike Yaruta <yarutamih@gmail.com>
2021-02-10 17:58:55 +0300

    Create a router of removing task from table

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,11 @@ const router = new Router();
 const loginRouter = require('./authRoutes/user.login.post');
 const regRouter = require('./authRoutes/user.register.post');
 const createTask = require('./tasksRoutes/task.create.post');
+const removeTask = require('./tasksRoutes/task.remove.post');
 
 router.use('/user', loginRouter);
 router.use('/user', regRouter);
 router.use('/list', createTask);
+router.use('/list', removeTask);
 
 module.exports = router;
--- routes/tasksRoutes/task.remove.post.js
+++ routes/tasksRoutes/task.remove.post.js
@@ -2,15 +2,16 @@ const Router = require('express');
 const router = new Router();
 const { Task } = require('../../models/task');
 
-router.post('/create', async (req, res) => {
+router.post('/remove', async (req, res) => {
   try {
-    console.log(req.body);
+    console.log('@@@@@@@ req.body:', req.body);
+    const { index } = req.body;
 
-    const { value } = req.body;
-
-    const task = await Task.create({ value });
-
-    console.log(task);
+    const task = Task.destroy({
+      where: {
+        id: index,
+      },
+    });
 
     return res.status(200).json(task);
   } catch (e) {
